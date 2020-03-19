9f16d2f4f94ce333ae278f60c45b0bbce3c3ad7a
Kateryna Boiko <katerina.boiko16@gmail.com>
2020-03-19 12:58:22 +0200

    lesson 2

---
+++ node_hw_2/routes/api/login.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const router = express.Router();
+
+const secret = require('../../config/auth').secret;
+
+let users = [
+    {id: 1, username: 'Kateryna', password: '123abc'},
+    {id: 2, username: 'Vova', password: '123456'}
+]
+router.post('/login', (req, res) => {
+    let {username, password} = req.body;
+
+    let [user] = users.filter(user => (user.username == username && user.password == password));
+
+    if(!user) {
+        res.status(401).json({status: 'User not found'});
+    }
+
+    let jwt_token = jwt.sign(user, secret);
+    res.json({jwt_token: jwt_token});
+});
+
+module.exports = router;
